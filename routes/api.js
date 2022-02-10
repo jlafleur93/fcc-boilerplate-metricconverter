97540a71be698522389039f89a6f95e7afff97b4
joelwowz <josephlafleur@hotmail.com>
2022-02-10 02:13:51 -0700

    init commit

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,17 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  
-  let convertHandler = new ConvertHandler();
-
+  const convertHandler = new ConvertHandler();
+  app.route("/api/convert").get(function (req, res) {
+    let convertedHandle = {};
+    let input = req.query.input;
+    let getNum = convertHandler.getNum(input);
+    let initUnit = convertHandler.getUnit(input);
+    convertedHandle["input"] = input;
+    convertedHandle["initUnit"] = initUnit;
+    res.json(convertedHandle);
+  });
 };
